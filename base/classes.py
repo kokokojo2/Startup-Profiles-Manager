45ba1df310974b3530f2852db517f7306e399354
Dmytro <foxgunplay@gmail.com>
2021-05-21 23:41:57 +0300

    created dataclasses of ProfileEntry and Profile

---
+++ base/classes.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ProfileEntry:
+    """
+    This class represents a single entry of a profile such as an app or etc.
+    """
+
+    name: str
+    priority: int
+    executable_path: str = None
+    id: int = None
+
+
+@dataclass
+class Profile:
+    """
+    This class represents a user-configured startup profile.
+    """
+
+    name: str
+    entries: list[ProfileEntry] = None
+    id: int = None
+
+
+
