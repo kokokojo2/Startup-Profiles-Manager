9079adc5c04f33b54bcdda50a4e8cd756eee31ac
Dmytro <foxgunplay@gmail.com>
2021-05-21 13:27:38 +0300

    DB manager skeleton created

--- db/db_manager.py
+++ db/db_manager.py
@@ -31,7 +31,7 @@ class DB:
             # TODO: add logs here
             pass
 
-    def create_structure(self):
+    def create_default_structure(self):
         self.get_cursor()
 
         self.cursor.executemany(
@@ -61,3 +61,67 @@ class DB:
             '''
         )
         self.close_connection()
+
+    def save_profile(self, profile_object):
+        """
+        Serializes profile obj with all entries and metadata to a database.
+        Assumes that id field marks whether profile exists in a database or not.
+        :param profile_object: Profile instance
+        :return:
+        """
+        pass
+
+    def save_profile_metadata(self, profile_object):
+        """
+        Inserts a new row to ProfileMeta. Should be used only if object does not yet exist in a database.
+        :param profile_object: Profile instance
+        :return:
+        """
+
+    def update_profile_metadata(self, profile_object):
+        """
+        Updates a row in ProfileMeta with profile_object.id. Should be used only if object exists in a database.
+        :param profile_object: Profile instance
+        :return:
+        """
+
+    def save_profile_entry(self, profile_entry):
+        """
+        Saves given profile entry.
+        :param profile_entry: named tuple with info about entry
+        :return:
+        """
+
+    def update_profile_entry(self, profile_entry):
+        """
+        Updates given profile entry.
+        :param profile_entry: named tuple with info about entry
+        :return:
+        """
+
+    def delete_profile_entry(self, profile_entry):
+        """
+        Deletes given profile entry.
+        :param profile_entry: named tuple with info about entry
+        :return:
+        """
+
+    def delete_profile(self, profile_object):
+        """
+        Deletes a row in ProfileMeta with profile_object.id and all associated profile entries.
+        :param profile_object: Profile instance
+        :return:
+        """
+
+    def get_profile_list(self):
+        """
+        Selects all profile metas from a database.
+        :return: list of profiles
+        """
+
+    def get_profile_entries(self, profile):
+        """
+        Selects all entries of a given profile.
+        :param profile: instance of Profile
+        :return: Profile object with entries inside
+        """
