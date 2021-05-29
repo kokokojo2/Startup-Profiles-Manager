59c29aca0d3cdb71012336d723aed63d502aecfd
Dmytro <foxgunplay@gmail.com>
2021-05-29 14:49:46 +0300

    added methods to execute exe file and launch all exe in profile to StartupManager

--- script/startup.py
+++ script/startup.py
@@ -1,4 +1,5 @@
 from subprocess import Popen
+from base.classes import ProfileEntry
 import logging
 
 # logging constants
@@ -33,3 +34,44 @@ class StartupManager:
         self.logger.addHandler(error_stream)
 
         self.logger.setLevel(logging.DEBUG)
+
+    def run_executable_from_path(self, profile_entry):
+        """
+        This method tells os to execute the executable file, path for which is specified intro profile_entry.executable_path.
+        :param profile_entry: Instance of ProfileEntry class
+        :return: True if file is launched successfully
+        """
+        self.logger.info(f'Trying to run executable from location - {profile_entry.executable_path}')
+
+        try:
+            result = Popen(profile_entry.executable_path)
+            self.logger.info(f'Executable is successfully launched with process id - {result.pid}.')
+            return True
+        except (FileNotFoundError, OSError) as e:
+
+            if isinstance(e, FileNotFoundError):
+                self.logger.warning(f'The application with name {profile_entry.name} has not been found at location {profile_entry.executable_path}. Please, update path in profile settings.')
+
+            if isinstance(e, OSError):
+                self.logger.warning(f'Oops, something went wrong with provided app. Check if the file on a specified location is an executable file.')
+
+            return False
+
+    def launch_profile(self, profile_obj):
+        """
+        This method launches every profile entry in a given Profile object, if profile_obj.disabled is not set to True.
+        :param profile_obj: instance of Profile class
+        :return: Number of success launches (for testing purposes).
+        """
+        self.logger.info('Launching profile...')
+        num_of_success_runs = 0
+
+        for entry in profile_obj.entries:
+            if not entry.disabled:
+                if self.run_executable_from_path(profile_obj):
+                    num_of_success_runs += 1
+
+            else:
+                self.logger.info(f'The profile entry with name - {entry.name} is disabled.')
+
+        return num_of_success_runs
