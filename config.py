127b891615963b51da8dfee6e1d634376332ef38
Dmytro <foxgunplay@gmail.com>
2021-05-29 17:22:49 +0300

    config added for the purpose of storing settings for different path of application in one place

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+# directory configuration
+
+APPLICATION_ROOT = os.getcwd()
+LOGS_DIR = os.path.join(APPLICATION_ROOT, 'logfiles')
+
+
+# logging configuration
+
+MAIN_LOG_FILENAME = 'main_log' + '.log'
+MESSAGE_FORMAT_TO_FILE = '%(asctime)s - %(name)s:%(levelname)s: in %(funcName)s: %(message)s'
+MESSAGE_FORMAT_TO_CONSOLE = '%(asctime)s - %(name)s:%(levelname)s: in %(funcName)s: %(message)s'
+
+DB_LOGGER_NAME = 'Database manager'
+STARTUP_LOGGER_NAME = 'Startup scripts'
+
+MAIN_LOG_PATH = os.path.join(LOGS_DIR, MAIN_LOG_FILENAME)
+
+
