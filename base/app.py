9d046e38e85fa3312d8b46d0d4307bb1bc318036
Dmytro <foxgunplay@gmail.com>
2021-05-31 00:07:35 +0300

    completed methods, added docstrings

--- base/app.py
+++ base/app.py
@@ -10,6 +10,12 @@ class Application:
     """
 
     def get_object_from_list(self, objects_list):
+        """
+        This function is used for requesting user to choose one item from a given list.
+        :param objects_list: list of objects to choose from
+        :return: chosen object
+        """
+
         for index, obj in enumerate(objects_list):
             print(f'[{index}][{obj.name}]')
 
@@ -21,7 +27,9 @@ class Application:
             print('Enter a valid index from a given list.')
 
     def run_profile(self):
-        # TODO: add docstring
+        """
+        Procedure that launches every enabled entry in a selected by user profile.
+        """
 
         while True:
             print('Select profile to launch:')
@@ -38,7 +46,9 @@ class Application:
                 break
 
     def create_profile(self):
-        # TODO: add docstring
+        """
+        Procedure that handles creation of profile with entries from user input.
+        """
 
         validator = Validator()
         db_manager = DB()
@@ -59,7 +69,10 @@ class Application:
         print(f'Profile saved to database. You can launch it any time from main menu.')
 
     def create_profile_entries(self):
-        # TODO: add docstring
+        """
+        Procedure that handles entries creation.
+        :return: list of created profile entries
+        """
 
         entries = []
         validator = Validator()
@@ -89,91 +102,127 @@ class Application:
         return entries
 
     def manage_profile(self):
-        # TODO: add docstring
+        """
+        Procedure that handles managing profile e.g. deleting, editing both profiles and entries and adding a new ones.
+        """
+
         db_manager = DB()
+        validator = Validator()
         profiles = db_manager.get_profile_list()
 
         while True:
             print('Select a profile: ')
-
             profile_obj = self.get_object_from_list(profiles)
 
             if profile_obj is not None:
-                print(profile_obj)
+                break
 
+        while True:
+            print(profile_obj)
+            print('[1][Edit name]\n[2][Add entry]\n[3][Edit entry]\n[4][Delete entry]\n[5][Delete profile]\n['
+                  '6][Exit to main menu]')
+            option = 0
+
+            try:
+                option = int(input('Choose one option: '))
+            except ValueError:
+                print('Enter a valid option.')
+
+            if option == 1:
                 while True:
-                    print('[1][Edit name]\n[2][Add entry]\n[3][Edit entry]\n[4][Delete entry]\n[5][Delete profile]\n['
-                          '6][Exit to main menu]')
-                    option = 0
-
-                    try:
-                        option = int(input('Choose one option: '))
-                    except ValueError:
-                        print('Enter a valid option.')
-
-                    if option == 1:
-                        new_name = input('Enter a new profile name: ')
-                        # TODO: add a loop for bad name
-                        # TODO: call validator for a name and save new obj
-                    if option == 2:
-                        # TODO: call add entry function and update profile
-                        pass
-                    if option == 3:
-                        while True:
-                            print('Choose entry to edit it:')
-                            entry = self.get_object_from_list(profile_obj.entries)
-                            if entry is not None:
-                                break
-                        self.edit_entry(entry)
-
-                    if option == 4:
-                        while True:
-                            print('Choose entry to delete it:')
-                            entry = self.get_object_from_list(profile_obj.entries)
-                            if entry is not None:
-                                break
-
-                        db_manager.delete_profile_entry(entry)
-
-                    if option == 5:
-                        option = input('Are you sure want to delete whole profile? [y/n]: ')
-
-                        if option == 'y':
-                            db_manager.delete_profile(profile_obj)
-
-                    if option == 5:
+                    new_name = input('Enter a new profile name: ')
+                    if validator.bool_validate_name(new_name):
                         break
 
+                profile_obj.name = new_name
+                db_manager.update_profile_metadata(profile_obj)
+
+            if option == 2:
+                new_entries = self.create_profile_entries()
+                profile_obj.entries += new_entries
+                db_manager.save_profile(profile_obj)
+
+            if option == 3:
+                while True:
+                    print('Choose entry to edit it:')
+                    entry = self.get_object_from_list(profile_obj.entries)
+                    if entry is not None:
+                        break
+                self.edit_entry(entry)
+
+            if option == 4:
+                while True:
+                    print('Choose entry to delete it:')
+                    entry = self.get_object_from_list(profile_obj.entries)
+                    if entry is not None:
+                        break
+
+                db_manager.delete_profile_entry(entry)
+
+            if option == 5:
+                option = input('Are you sure want to delete whole profile? [y/n]: ')
+
+                if option == 'y':
+                    db_manager.delete_profile(profile_obj)
+                    print('Profile was deleted.')
+                    return
+
+            if option == 6:
+                break
+
+            if option == 3 or option == 4:
+                profile_obj.entries = db_manager.get_profile_entries(profile_obj)
+
     def edit_entry(self, entry):
+        """
+        Procedure that handles editing of a profile entry fields. Is a sub-procedure of manage_profile procedure.
+        :param entry: instance of ProfileEntry object that should be edited
+        """
+
         while True:
             print('Choose one option:\n[1][Change name]\n[2][Change priority]\n[3][Change path to '
-                  'exe][4][Enable\\Disable]\n')
+                  'exe][4][Enable/Disable]\n[5][Exit to previous menu]\n')
             option = 0
             db_manager = DB()
+            validator = Validator()
 
             try:
                 option = int(input())
             except ValueError:
                 print('Enter a valid option.')
 
-            # TODO: add whiles if mistakes
             if option == 1:
-                new_name = input('Enter new name for a profile: ')
-                # TODO: call validator and save new obj
+                while True:
+                    new_name = input('Enter new name for a profile: ')
+                    if validator.bool_validate_name(new_name):
+                        break
+
+                entry.name = new_name
 
             if option == 2:
-                new_priority = input('Enter new priority as an integer: ')
-                # TODO: call validator and save new obj
+                while True:
+                    new_priority = validator.get_valid_priority(input('Enter new priority as an integer: '))
+                    if new_priority is not None:
+                        break
+
+                entry.priority = new_priority
 
             if option == 3:
-                new_path = input('Enter new path using "\\" to distinguish locations: ')
-                # TODO: call validator and save new obj
+                while True:
+                    new_path = validator.get_valid_path(input('Enter new path using "\\" to distinguish locations: '))
+                    if new_path is not None:
+                        break
+
+                entry.executable_path = new_path
 
             if option == 4:
                 option = input(
                     f'This entry is now {"disabled" if entry.disabled else "enabled"}. Change to the opposite? [y/n]')
                 if option == 'y':
                     entry.disabled = not entry.disabled
+
+            if option == 5:
+                break
 
             db_manager.update_profile_entry(entry)
 
