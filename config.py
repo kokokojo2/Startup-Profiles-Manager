37bff2eb4164343be53922d4e020a88235923ad6
Dmytro <foxgunplay@gmail.com>
2021-05-30 14:36:13 +0300

    Added logger name for Validator class

--- config.py
+++ config.py
@@ -11,10 +11,12 @@ LOGS_DIR = os.path.join(APPLICATION_ROOT, 'logfiles')
 
 MAIN_LOG_FILENAME = 'main_log' + '.log'
 MESSAGE_FORMAT_TO_FILE = '%(asctime)s - %(name)s:%(levelname)s: in %(funcName)s: %(message)s'
+# TODO: change to contain only user significant data
 MESSAGE_FORMAT_TO_CONSOLE = '%(asctime)s - %(name)s:%(levelname)s: in %(funcName)s: %(message)s'
 
 DB_LOGGER_NAME = 'Database manager'
 STARTUP_LOGGER_NAME = 'Startup scripts'
+VALIDATOR_LOGGER_NAME = 'Validator'
 
 MAIN_LOG_PATH = os.path.join(LOGS_DIR, MAIN_LOG_FILENAME)
 
