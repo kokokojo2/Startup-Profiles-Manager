7c89ffcfd1407cdfe6751ed3fe4ae947a2234f1f
Dmytro <foxgunplay@gmail.com>
2021-06-11 01:16:05 +0300

    bug with not displaying important information fixed

--- base/app.py
+++ base/app.py
@@ -47,6 +47,7 @@ class Application:
         """
         if len(objects_list) == 0:
             print('There is no items to choose from. Please, create one first.')
+            input('Press enter to exit to previous menu.')
             return -1
 
         else:
@@ -94,7 +95,7 @@ class Application:
                 input('Press enter to go back to menu.')
                 break
 
-        if settings_manager.get_settings().close_after_launch:
+        if settings_manager.get_settings().close_after_launch and not isinstance(profile_obj, int):
             print('This program is configured to finish automatically after profile launch.')
             print('It will finish in:\n')
             for i in reversed(range(3)):
