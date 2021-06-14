a1c8f6d241fb315cf10d8b67ac13d4f0b8a9b99a
Dmytro <foxgunplay@gmail.com>
2021-06-14 10:44:39 +0300

    eel test added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import eel
+
+
 from base.app import Application
 from base.service import IntegrityChecker
 
 if __name__ == '__main__':
-    checker = IntegrityChecker()
-    checker.check()
-
-    app = Application()
-    app.run()
+    eel.init('front')
+    eel.start('main_menu.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
