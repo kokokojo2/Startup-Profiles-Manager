9f56cfb2662c59992732d0818737d06c63ca1581
Dmytro <foxgunplay@gmail.com>
2021-06-19 22:25:24 +0300

    integrity checker job added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import eel
 import app.mid_level_func
+from base.service import IntegrityChecker
 
 if __name__ == '__main__':
+    checker = IntegrityChecker()
+    checker.check()
+
     eel.init('front')
     eel.start('templates/detail_profile.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'],
               jinja_templates='templates')
