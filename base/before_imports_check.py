a5e5c7afc6601bce8e1aeafb40ed3c6128203b53
Dmytro <foxgunplay@gmail.com>
2021-07-02 09:47:11 +0300

    added check for root folder existence

--- base/before_imports_check.py
+++ base/before_imports_check.py
@@ -3,9 +3,15 @@ import config
 
 
 def check():
+    check_root_folder_existence()
     check_log_folder_existence()
 
 
 def check_log_folder_existence():
     if not os.path.exists(config.LOGS_DIR):
         os.makedirs(config.LOGS_DIR)
+
+
+def check_root_folder_existence():
+    if not os.path.exists(config.APPLICATION_ROOT):
+        os.makedirs(config.APPLICATION_ROOT)
