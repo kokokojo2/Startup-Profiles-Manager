b239cbe337e4d84234e36bcd795cc5db03a93f94
Dmytro <foxgunplay@gmail.com>
2021-07-02 13:28:34 +0300

    path to electron app changed to use build electron version

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     checker.check()
 
     eel.init('front')
-    eel.start('templates/detail_profile.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'],
+    eel.start('templates/detail_profile.html', mode='custom', cmdline_args=['electron.exe', '.'],
               jinja_templates='templates')
